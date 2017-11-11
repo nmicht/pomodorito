61c031f086e8901d85b9b60c9cd14159094a494a
Michelle Torres <michelle.torres@corb.mx>
2017-11-11 07:34:53 -0600

    Render Counter component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Counter from './components/Counter';
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,11 +9,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Pomodorito</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Counter />
       </div>
     );
   }
